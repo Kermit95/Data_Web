01b9ce8e41b973f4ae0989a2faa4b77941c875dd
Kermit <kermitwang95@gmail.com>
2016-11-22 23:57:51 +0800

    rm sensitive info

--- sql/create_and_identify.sql
+++
@@ -1,5 +0,0 @@
-drop database if exists `data_project`;
-create database `data_project` default character set utf8;
-
-grant all privileges on data_project.* to 'kermit'@'localhost' identified by '1234qwer';
-flush privileges;
